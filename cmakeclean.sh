a98c499079c51ca362367a105825163ad7030641
Robert J. Hansen <rjh@sixdemonbag.org>
2016-11-05 02:09:16 -0400

    replaced by clean.py

--- cmakeclean.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rm -rf CMakeCache.txt CMakeFiles cmake_install.cmake CPackConfig.cmake CPackSourceConfig.cmake src/CMakeCache.txt src/CMakeFiles src/cmake_install.cmake src/CPackConfig.cmake src/CPackSourceConfig.cmake src/*.o src/nsrllookup src/nsrllookup.exe
